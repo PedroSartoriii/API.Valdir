0f7770cf6eee3d0f923892a0dc8ba45bb3ad3a25
PedroSartoriii <pedrosartorii@hotmail.com>
2023-09-26 09:42:11 -0300

    Commit

---
+++ projetos/RickyAndMorty/src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+import {ref} from 'vue'
+let num = ref(0)
+</script>
+
+  <template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark border-bottom border-body">
+      <div class="container-fluid">
+        <div class="p-2 flex-fill bd-highlight"></div>
+        <router-link to="/" class="navbar-brand">Ricky and Morty</router-link>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls = "navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  <div class="main">
+    <RouterView></RouterView>
+  </div>
+  <footer class="bg-dark text-light">
+    <div> API | Desenvolvimento de Aplicações para a Internet |{{ new Date().getFullYear() }} </div>
+  </footer>
+  </template>
+
+
+<style>
+  footer{
+    position:fixed;
+    bottom:0;
+    width:100%;
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    height:30px;
+  }
+</style>
+
---
+++ projetos/RickyAndMorty/src/views/HomeView.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { onMounted, reactive, ref } from 'vue';
+import { RouterLink } from 'vue-router';
+
+let character = reactive(ref());
+
+onMounted(() => {
+	fetch("https://rickandmortyapi.com/api/character").then(res => { res.json().then((x) => { character = x.results; console.log(character); }) })
+})
+
+</script>
+<template>
+  <div class="row">
+    <div class="row col-35">
+      <div v-for="c in character" class="col-6 d-flex justify-content-center">
+        <div class="col-sm-20 col-md-8">
+          <div class="card">
+            <div class="card-body row">
+              <div class=" "><strong>{{ c.name }}</strong></div>
+              <img :src="c.image" >
+                <div class="d-flex bd-highlight">
+                  <div class="p-2 flex-fill bd-highlight"><strong>Espécie:</strong> {{ c.species }}</div>
+                  <div class="p-2 flex-fill bd-highlight"><strong>Sexo:</strong> {{ c.gender }}</div>
+                  <div class="p-2 flex-fill bd-highlight"><strong>Situação:</strong> {{ c.status }}</div>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+body {
+    margin: 0;
+    padding: 0;
+    background-color: #7aec7a; 
+}
+</style>
